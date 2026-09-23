0fb1e6aa1c7b477b45a2183162c50c7e59c12f7b
cadexleader-lgtm <2.64957485e+08+cadexleader-lgtm@users.noreply.github.com>
2026-09-23 10:18:05 +0000

    Restore accessible horizontal image carousel

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -448,11 +448,7 @@ export default function Home() {
         <div className="relative">
           <div className="absolute left-0 top-0 bottom-0 w-32 bg-gradient-to-r from-background to-transparent z-10"></div>
           <div className="absolute right-0 top-0 bottom-0 w-32 bg-gradient-to-l from-background to-transparent z-10"></div>
-          <motion.div 
-            className="flex gap-4"
-            animate={{ x: [0, -1920] }}
-            transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
-          >
+          <div className="secureflow-carousel-track flex gap-4">
             {[...carouselImages, ...carouselImages].map((img, i) => (
               <div key={i} className="shrink-0 w-48 h-32 md:w-64 md:h-44 rounded-xl overflow-hidden border border-white/5 hover:border-primary/30 transition-all group">
                 <img 
@@ -462,7 +458,7 @@ export default function Home() {
                 />
               </div>
             ))}
-          </motion.div>
+          </div>
         </div>
       </section>
 
